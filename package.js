5d3d211ecdaaf9df66a9094db376b7b49cc363a8
hhravn <hhravn@gmail.com>
2015-10-02 11:38:49 +0200

    atmosphere package deps

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 var packageName = 'hhravn:angular-ui-codemirror';
 var where = 'client'; // where to install: 'client' or 'server'. For both, pass nothing.
-var version = '0.3.0';
+var version = '0.3.1';
 var summary = 'This directive allows you to add CodeMirror to your textarea elements.';
 var gitLink = 'https://github.com/hhravn/ui-codemirror';
 var documentationFile = 'README.md';
@@ -16,5 +16,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom(['METEOR@0.9.0', 'METEOR@1.0']); // Meteor versions
+  api.addFiles('./bower_components/codemirror/lib/codemirror.css', where); // Files in use
+  api.addFiles('./bower_components/codemirror/lib/codemirror.js', where); // Files in use
   api.addFiles('./src/ui-codemirror.js', where); // Files in use
 });  
